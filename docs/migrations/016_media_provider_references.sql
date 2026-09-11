a2270bc46cd81f40840171ccafda505edf230792
Abaan Shujaat <abaanshujat212@gmail.com>
2026-09-11 10:39:08 +0500

    fix: deduplicate media attachment recovery per message

--- docs/migrations/016_media_provider_references.sql
+++ docs/migrations/016_media_provider_references.sql
@@ -1,3 +1,7 @@
 ALTER TABLE message_attachments
   ADD COLUMN provider_connection_id TEXT REFERENCES provider_connections(id) ON DELETE SET NULL,
   ADD COLUMN provider_media_id TEXT;
+
+CREATE UNIQUE INDEX uq_message_attachment_message_provider_media
+  ON message_attachments(message_id, provider_media_id)
+  WHERE provider_media_id IS NOT NULL;
