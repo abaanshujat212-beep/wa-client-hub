1c1faef9c44f6aad6fc45fb5f90e707ae862b450
Abaan Shujaat <abaanshujat212@gmail.com>
2026-09-11 10:38:25 +0500

    fix: allow reused provider media references

--- docs/migrations/016_media_provider_references.sql
+++ docs/migrations/016_media_provider_references.sql
@@ -1,7 +1,3 @@
 ALTER TABLE message_attachments
   ADD COLUMN provider_connection_id TEXT REFERENCES provider_connections(id) ON DELETE SET NULL,
   ADD COLUMN provider_media_id TEXT;
-
-CREATE UNIQUE INDEX uq_message_attachment_provider_media
-  ON message_attachments(provider_connection_id, provider_media_id)
-  WHERE provider_connection_id IS NOT NULL AND provider_media_id IS NOT NULL;
